e75695a7815db6d3a03cfd383a22d380cd51390a
David Eberly <deberly@geometrictools.com>
2021-04-23 12:33:42 -0700

    Update Hyperplane.h
    The Hyperplane constructor whose input is a std::array of points uses singular value decomposition to build the plane through the points. The body has been replaced by a call to a function that is determined at compile time for dimension 3 and for other dimensions. This is a hack based on a bug report for the code executed on a QEMU/KVM virtual machine, which indicated the singular value decomposition was producing inaccurate results. I am unable to reproduce the problem on a non-virtual machine; the SVD works correctly for the dataset included in the bug report. I need to determine what the virtual machine is doing that causes such inaccurate results when using floating-point arithmetic. (The use of SVD in 3D is overkill anyway...)

--- GTE/Mathematics/Hyperplane.h
+++ GTE/Mathematics/Hyperplane.h
@@ -3,12 +3,13 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 // https://www.geometrictools.com/License/Boost/LICENSE_1_0.txt
-// Version: 4.0.2019.08.13
+// Version: 4.0.2021.04.23
 
 #pragma once
 
 #include <Mathematics/Matrix.h>
 #include <Mathematics/SingularValueDecomposition.h>
+#include <Mathematics/Vector3.h>
 
 // The plane is represented as Dot(U,X) = c where U is a unit-length normal
 // vector, c is the plane constant, and X is any point on the plane.  The user
@@ -50,19 +51,7 @@ namespace gte
         // pointson the hyperplane.
         Hyperplane(std::array<Vector<N, Real>, N> const& p)
         {
-            Matrix<N, N - 1, Real> edge;
-            for (int i = 0; i < N - 1; ++i)
-            {
-                edge.SetCol(i, p[i + 1] - p[0]);
-            }
-
-            // Compute the 1-dimensional orthogonal complement of the edges of
-            // the simplex formed by the points p[].
-            SingularValueDecomposition<Real> svd(N, N - 1, 32);
-            svd.Solve(&edge[0], -1);
-            svd.GetUColumn(N - 1, &normal[0]);
-
-            constant = Dot(normal, p[0]);
+            ComputeFromPoints<N>(p);
         }
 
         // Public member access.
@@ -109,6 +98,46 @@ namespace gte
         bool operator>=(Hyperplane const& hyperplane) const
         {
             return !operator<(hyperplane);
+        }
+
+    private:
+        // TODO: This is used in the
+        // Hyperplane(std::array<Vector<N, Real>, N> const&) constructor to
+        // have separate implementations for N = 3 and N != 3. A bug report
+        // was filed for that constructor with code executed on a QEMU/KVM
+        // virtual machine, which indicated the singular value decomposition
+        // was producing inaccurate results. I am unable to reproduce the
+        // problem on a non-virtual machine; the SVD works correctly for the
+        // dataset included in the bug report. I need to determine what the
+        // virtual machine is doing that causes such inaccurate results when
+        // using floating-point arithmetic.
+        template <int Dimension = N>
+        typename std::enable_if<Dimension != 3, void>::type
+        ComputeFromPoints(std::array<Vector<Dimension, Real>, Dimension> const& p)
+        {
+            Matrix<Dimension, Dimension - 1, Real> edge;
+            for (int i = 0; i < Dimension - 1; ++i)
+            {
+                edge.SetCol(i, p[i + 1] - p[0]);
+            }
+
+            // Compute the 1-dimensional orthogonal complement of the edges of
+            // the simplex formed by the points p[].
+            SingularValueDecomposition<Real> svd(Dimension, Dimension - 1, 32);
+            svd.Solve(&edge[0], -1);
+            svd.GetUColumn(Dimension - 1, &normal[0]);
+
+            constant = Dot(normal, p[0]);
+        }
+
+        template <int Dimension = N>
+        typename std::enable_if<Dimension == 3, void>::type
+        ComputeFromPoints(std::array<Vector<Dimension, Real>, Dimension> const& p)
+        {
+            Vector<Dimension, Real> edge0 = p[1] - p[0];
+            Vector<Dimension, Real> edge1 = p[2] - p[0];
+            normal = UnitCross(edge0, edge1);
+            constant = Dot(normal, p[0]);
         }
     };
 
