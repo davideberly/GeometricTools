9dd6e1dac074be1e8709d7be312847a742ff3947
David Eberly <deberly@geometrictools.com>
2021-05-21 10:00:56 -0700

    Update Minimize1.h
    Fixed a bug in the subdivision logic. The logic itself was simplified to be compact and more readable.

--- GTE/Mathematics/Minimize1.h
+++ GTE/Mathematics/Minimize1.h
@@ -3,258 +3,181 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 // https://www.geometrictools.com/License/Boost/LICENSE_1_0.txt
-// Version: 4.0.2019.08.13
+// Version: 4.0.2021.05.21
 
 #pragma once
 
 #include <Mathematics/Logger.h>
 #include <Mathematics/Math.h>
 #include <algorithm>
+#include <cstdint>
 #include <functional>
 
-// The interval [t0,t1] provided to GetMinimum(Real,Real,Real,Real&,Real&)
-// is processed by examining subintervals.  On each subinteral [a,b], the
-// values f0 = F(a), f1 = F((a+b)/2), and f2 = F(b) are examined.  If
-// {f0,f1,f2} is monotonic, then [a,b] is subdivided and processed.  The
-// maximum depth of the recursion is limited by 'maxLevel'.  If {f0,f1,f2}
-// is not monotonic, then two cases arise.  First, if f1 = min{f0,f1,f2},
-// then {f0,f1,f2} is said to "bracket a minimum" and GetBracketedMinimum(*)
-// is called to locate the function minimum.  The process uses a form of
-// bisection called "parabolic interpolation" and the maximum number of
-// bisection steps is 'maxBracket'.  Second, if f1 = max{f0,f1,f2}, then
-// {f0,f1,f2} brackets a maximum.  The minimum search continues recursively
-// as before on [a,(a+b)/2] and [(a+b)/2,b].
+// Search for a minimum of F(t) on [t0,t1] using successive parabolic
+// interpolation. The search is recursive based on the polyline associated
+// with (t,F(t)) at the endpoints and the midpoint of an interval. Let
+// f0 = F(t0), f1 = F(t1), tm is in (t0,t1) and fm = F(tm). The polyline is
+// {(t0,f0),(tm,fm),(t1,f1)}.
+//
+// If the polyline is V-shaped, the interval [t0,t1] contains a minimum
+// point. The polyline is fit with a parabola whose vertex tv is in (t0,t1).
+// Let fv = tv. If {(t0,f0),(tv,fv),(tm,fm)}} is a minimum bracket, the
+// parabolic interpolation continues in [t0,tm]. If instead
+// {(tm,fm),(tv,fv),(t1,f1)}} is a minimum bracket, the parabolic
+// interpolation continues in [tm,t1].
+//
+// If the polyline is not V-shaped, both subintervals [t0,tm] and [tm,t1]
+// are searched for a minimum.
 
 namespace gte
 {
-    template <typename Real>
+    template <typename T>
     class Minimize1
     {
     public:
         // Construction.
-        Minimize1(std::function<Real(Real)> const& F, int maxLevel, int maxBracket,
-            Real epsilon = (Real)1e-08, Real tolerance = (Real)1e-04)
+        Minimize1(std::function<T(T)> const& F, int32_t maxSubdivisions,
+            int32_t maxBisections, T epsilon = static_cast<T>(1e-08),
+            T tolerance = static_cast<T>(1e-04))
             :
             mFunction(F),
-            mMaxLevel(maxLevel),
-            mMaxBracket(maxBracket),
-            mEpsilon(0),
-            mTolerance(0)
+            mMaxSubdivisions(maxSubdivisions),
+            mMaxBisections(maxBisections),
+            mEpsilon(std::max(epsilon, static_cast<T>(0))),
+            mTolerance(std::max(tolerance, static_cast<T>(0)))
         {
+            LogAssert(
+                mMaxSubdivisions > 0 && maxBisections > 0,
+                "Invalid argument.");
+
             SetEpsilon(epsilon);
             SetTolerance(tolerance);
         }
 
         // Member access.
-        inline void SetEpsilon(Real epsilon)
+        inline void SetEpsilon(T epsilon)
         {
-            mEpsilon = (epsilon > (Real)0 ? epsilon : (Real)0);
+            mEpsilon = std::max(epsilon, static_cast<T>(0));
         }
 
-        inline void SetTolerance(Real tolerance)
+        inline void SetTolerance(T tolerance)
         {
-            mTolerance = (tolerance > (Real)0 ? tolerance : (Real)0);
+            mTolerance = std::max(tolerance, static_cast<T>(0));
         }
 
-        inline Real GetEpsilon() const
+        inline T GetEpsilon() const
         {
             return mEpsilon;
         }
 
-        inline Real GetTolerance() const
+        inline T GetTolerance() const
         {
             return mTolerance;
         }
 
-        // Search for a minimum of 'function' on the interval [t0,t1] using an
-        // initial guess of 'tInitial'.  The location of the minimum is 'tMin'
-        // and/ the value of the minimum is 'fMin'.
-        void GetMinimum(Real t0, Real t1, Real tInitial, Real& tMin, Real& fMin)
+        // Search for a minimum of F(t) on the interval [t0,t1] using an
+        // initial guess of (t0+t1)/2. The location of the minimum is tMin
+        // and the value of the minimum is fMin = F(tMin).
+        void GetMinimum(T t0, T t1, T& tMin, T& fMin)
         {
-            LogAssert(t0 <= tInitial && tInitial <= t1, "Invalid initial t value.");
+            GetMinimum(t0, t1, static_cast<T>(0.5) * (t0 + t1), tMin, fMin);
+        }
 
-            mTMin = std::numeric_limits<Real>::max();
-            mFMin = std::numeric_limits<Real>::max();
+        // Search for a minimum of F(t) on the interval [t0,t1] using an
+        // initial guess of tInitial. The location of the minimum is tMin
+        // and the value of the minimum is fMin = F(tMin).
+        void GetMinimum(T t0, T t1, T tInitial, T& tMin, T& fMin)
+        {
+            LogAssert(
+                t0 <= tInitial && tInitial <= t1,
+                "Invalid initial t value.");
 
-            Real f0 = mFunction(t0);
+            // Compute the minimum for the 3 initial points.
+            mTMin = std::numeric_limits<T>::max();
+            mFMin = std::numeric_limits<T>::max();
+
+            T f0 = mFunction(t0);
             if (f0 < mFMin)
             {
                 mTMin = t0;
                 mFMin = f0;
             }
 
-            Real fInitial = mFunction(tInitial);
+            T fInitial = mFunction(tInitial);
             if (fInitial < mFMin)
             {
                 mTMin = tInitial;
                 mFMin = fInitial;
             }
 
-            Real f1 = mFunction(t1);
+            T f1 = mFunction(t1);
             if (f1 < mFMin)
             {
                 mTMin = t1;
                 mFMin = f1;
             }
 
-            GetMinimum(t0, f0, tInitial, fInitial, t1, f1, mMaxLevel);
+            // Search for the global minimum on [t0,t1] with tInitial chosen
+            // hopefully to start with a minimum bracket.
+            if (((fInitial < f0) && (f1 >= fInitial)) ||
+                ((f1 > fInitial) && (f0 >= fInitial)))
+            {
+                // The polyline {(f0,f0), (tInitial,fInitial), (t1,f1)} is V-shaped.
+                GetBracketedMinimum(t0, f0, tInitial, fInitial, t1, f1, mMaxSubdivisions);
+            }
+            else
+            {
+                // The polyline {(f0,f0), (tInitial,fInitial), (t1,f1)} is not
+                // V-shaped, so continue searching in subintervals
+                // [t0,tInitial] and [tInitial,t1].
+                Subdivide(t0, f0, tInitial, fInitial, mMaxSubdivisions);
+                Subdivide(tInitial, fInitial, t1, f1, mMaxSubdivisions);
+            }
 
             tMin = mTMin;
             fMin = mFMin;
         }
 
     private:
-        // This is called to start the search on [t0,tInitial] and
-        // [tInitial,t1].
-        void GetMinimum(Real t0, Real f0, Real t1, Real f1, int level)
+        // Search [t0,t1] recursively for a global minimum.
+        void Subdivide(T t0, T f0, T t1, T f1, int32_t subdivisionsRemaining)
         {
-            if (level-- == 0)
+            if (subdivisionsRemaining-- == 0)
             {
+                // The maximum number of subdivisions has been reached.
                 return;
             }
 
-            Real tm = (Real)0.5 * (t0 + t1);
-            Real fm = mFunction(tm);
+            // Compute the function at the midpoint of [t0,t1].
+            T tm = static_cast<T>(0.5) * (t0 + t1);
+            T fm = mFunction(tm);
             if (fm < mFMin)
             {
                 mTMin = tm;
                 mFMin = fm;
             }
 
-            if (f0 - (Real)2 * fm + f1 > (Real)0)
+            if (((fm < f0) && (f1 >= fm)) || ((f1 > fm) && (f0 >= fm)))
             {
-                // The quadratic fit has positive second derivative at the
-                // midpoint.
-                if (f1 > f0)
-                {
-                    if (fm >= f0)
-                    {
-                        // Increasing, repeat on [t0,tm].
-                        GetMinimum(t0, f0, tm, fm, level);
-                    }
-                    else
-                    {
-                        // Not monotonic, have a bracket.
-                        GetBracketedMinimum(t0, f0, tm, fm, t1, f1, level);
-                    }
-                }
-                else if (f1 < f0)
-                {
-                    if (fm >= f1)
-                    {
-                        // Decreasing, repeat on [tm,t1].
-                        GetMinimum(tm, fm, t1, f1, level);
-                    }
-                    else
-                    {
-                        // Not monotonic, have a bracket.
-                        GetBracketedMinimum(t0, f0, tm, fm, t1, f1, level);
-                    }
-                }
-                else
-                {
-                    // Constant, repeat on [t0,tm] and [tm,t1].
-                    GetMinimum(t0, f0, tm, fm, level);
-                    GetMinimum(tm, fm, t1, f1, level);
-                }
+                // The polyline {(f0,f0), (tm,fm), (t1,f1)} is V-shaped.
+                GetBracketedMinimum(t0, f0, tm, fm, t1, f1, subdivisionsRemaining);
             }
             else
             {
-                // The quadratic fit has nonpositive second derivative at the
-                // midpoint.
-                if (f1 > f0)
-                {
-                    // Repeat on [t0,tm].
-                    GetMinimum(t0, f0, tm, fm, level);
-                }
-                else if (f1 < f0)
-                {
-                    // Repeat on [tm,t1].
-                    GetMinimum(tm, fm, t1, f1, level);
-                }
-                else
-                {
-                    // Repeat on [t0,tm] and [tm,t1].
-                    GetMinimum(t0, f0, tm, fm, level);
-                    GetMinimum(tm, fm, t1, f1, level);
-                }
-            }
-        }
-
-        // This is called recursively to search [a,(a+b)/2] and [(a+b)/2,b].
-        void GetMinimum(Real t0, Real f0, Real tm, Real fm, Real t1, Real f1, int level)
-        {
-            if (level-- == 0)
-            {
-                return;
-            }
-
-            if ((t1 - tm) * (f0 - fm) > (tm - t0) * (fm - f1))
-            {
-                // The quadratic fit has positive second derivative at the
-                // midpoint.
-                if (f1 > f0)
-                {
-                    if (fm >= f0)
-                    {
-                        // Increasing, repeat on [t0,tm].
-                        GetMinimum(t0, f0, tm, fm, level);
-                    }
-                    else
-                    {
-                        // Not monotonic, have a bracket.
-                        GetBracketedMinimum(t0, f0, tm, fm, t1, f1, level);
-                    }
-                }
-                else if (f1 < f0)
-                {
-                    if (fm >= f1)
-                    {
-                        // Decreasing, repeat on [tm,t1].
-                        GetMinimum(tm, fm, t1, f1, level);
-                    }
-                    else
-                    {
-                        // Not monotonic, have a bracket.
-                        GetBracketedMinimum(t0, f0, tm, fm, t1, f1, level);
-                    }
-                }
-                else
-                {
-                    // Constant, repeat on [t0,tm] and [tm,t1].
-                    GetMinimum(t0, f0, tm, fm, level);
-                    GetMinimum(tm, fm, t1, f1, level);
-                }
-            }
-            else
-            {
-                // The quadratic fit has a nonpositive second derivative at
-                // the midpoint.
-                if (f1 > f0)
-                {
-                    // Repeat on [t0,tm].
-                    GetMinimum(t0, f0, tm, fm, level);
-                }
-                else if (f1 < f0)
-                {
-                    // Repeat on [tm,t1].
-                    GetMinimum(tm, fm, t1, f1, level);
-                }
-                else
-                {
-                    // Repeat on [t0,tm] and [tm,t1].
-                    GetMinimum(t0, f0, tm, fm, level);
-                    GetMinimum(tm, fm, t1, f1, level);
-                }
+                // The polyline {(f0,f0), (tm,fm), (t1,f1)} is not V-shaped,
+                // so continue searching in subintervals [t0,tm] and [tm,t1].
+                Subdivide(t0, f0, tm, fm, subdivisionsRemaining);
+                Subdivide(tm, fm, t1, f1, subdivisionsRemaining);
             }
         }
 
         // This is called when {f0,f1,f2} brackets a minimum.
-        void GetBracketedMinimum(Real t0, Real f0, Real tm, Real fm, Real t1, Real f1, int level)
+        void GetBracketedMinimum(T t0, T f0, T tm, T fm, T t1, T f1,
+            int32_t subdivisionsRemaining)
         {
-            for (int i = 0; i < mMaxBracket; ++i)
+            for (int32_t i = 0; i < mMaxBisections; ++i)
             {
-                // Update minimum value.
+                // Update the minimum location and value.
                 if (fm < mFMin)
                 {
                     mTMin = tm;
@@ -262,19 +185,21 @@ namespace gte
                 }
 
                 // Test for convergence.
-                if (std::fabs(t1 - t0) <= (Real)2 * mTolerance * std::fabs(tm) + mEpsilon)
+                T dt10 = t1 - t0;
+                T dtBound = static_cast<T>(2) * mTolerance * std::fabs(tm) + mEpsilon;
+                if (dt10 <= dtBound)
                 {
                     break;
                 }
 
-                // Compute vertex of interpolating parabola.
-                Real dt0 = t0 - tm;
-                Real dt1 = t1 - tm;
-                Real df0 = f0 - fm;
-                Real df1 = f1 - fm;
-                Real tmp0 = dt0 * df1;
-                Real tmp1 = dt1 * df0;
-                Real denom = tmp1 - tmp0;
+                // Compute the vertex of the interpolating parabola.
+                T dt0m = t0 - tm;
+                T dt1m = t1 - tm;
+                T df0m = f0 - fm;
+                T df1m = f1 - fm;
+                T tmp0 = dt0m * df1m;
+                T tmp1 = dt1m * df0m;
+                T denom = tmp1 - tmp0;
                 if (std::fabs(denom) <= mEpsilon)
                 {
                     return;
@@ -282,9 +207,9 @@ namespace gte
 
                 // Compute tv and clamp to [t0,t1] to offset floating-point
                 // rounding errors.
-                Real tv = tm + (Real)0.5 * (dt1 * tmp1 - dt0 * tmp0) / denom;
+                T tv = tm + static_cast<T>(0.5) * (dt1m * tmp1 - dt0m * tmp0) / denom;
                 tv = std::max(t0, std::min(tv, t1));
-                Real fv = mFunction(tv);
+                T fv = mFunction(tv);
                 if (fv < mFMin)
                 {
                     mTMin = tv;
@@ -323,17 +248,19 @@ namespace gte
                 }
                 else
                 {
-                    // The vertex of parabola is already at middle sample point.
-                    GetMinimum(t0, f0, tm, fm, level);
-                    GetMinimum(tm, fm, t1, f1, level);
+                    // The vertex of parabola is located at the middle sample
+                    // point. A global minimum could occur on either
+                    // subinterval.
+                    Subdivide(t0, f0, tm, fm, subdivisionsRemaining);
+                    Subdivide(tm, fm, t1, f1, subdivisionsRemaining);
                 }
             }
         }
 
-        std::function<Real(Real)> mFunction;
-        int mMaxLevel;
-        int mMaxBracket;
-        Real mTMin, mFMin;
-        Real mEpsilon, mTolerance;
+        std::function<T(T)> mFunction;
+        int32_t mMaxSubdivisions;
+        int32_t mMaxBisections;
+        T mTMin, mFMin;
+        T mEpsilon, mTolerance;
     };
 }
