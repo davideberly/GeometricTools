b51c7fb49edac00fa90a0b810e4cdc55fbb94585
David Eberly <deberly@geometrictools.com>
2021-11-02 15:27:23 -0700

    Update TriangulateCDT.h
    In FillRegion, accessing tkey from the front of regionQueue using a const reference is incorrect, because the next line of code is a pop of the queue which invalidates the memory, corrupting the value of tkey. A copy must be made of the front of the queue. Thanks to Aaron Barony for tracking down the problem.

--- GTE/Mathematics/TriangulateCDT.h
+++ GTE/Mathematics/TriangulateCDT.h
@@ -3,7 +3,7 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 // https://www.geometrictools.com/License/Boost/LICENSE_1_0.txt
-// Version: 4.0.2021.04.22
+// Version: 4.0.2021.11.02
 
 #pragma once
 
@@ -988,7 +988,7 @@ namespace gte
             auto const& tmap = graph.GetTriangles();
             while (regionQueue.size() > 0)
             {
-                auto const& tkey = regionQueue.front();
+                TriangleKey<true> tkey = regionQueue.front();
                 regionQueue.pop();
                 auto titer = tmap.find(tkey);
                 LogAssert(titer != tmap.end(), "Unexpected condition.");
