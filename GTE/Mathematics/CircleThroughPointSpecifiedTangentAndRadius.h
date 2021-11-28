ea207c54f1cd30ae5976847d2a1ad44fe7ebec73
David Eberly <deberly@geometrictools.com>
2021-11-28 15:25:48 -0800

    Added implementations for Sections 8.6 and 8.7 of the "Geometric Tools for Computer Graphics" book.
    Added implementations of two algorithms in "Geometric Tools for Computer Graphics" (Sections 8.6 and 8.7) at user request. The implementation for the algorithm of Section 8.6 differs from that of the book--a simpler approach is used, described in the file comments. The book has pseudocode for the algorithm in Section 8.7, but there are numerous errors. I have added the corrections to the book correction page at the website. The approach I used is described in the file comments.

---
+++ GTE/Mathematics/CircleThroughPointSpecifiedTangentAndRadius.h
@@ -0,0 +1,154 @@
+// David Eberly, Geometric Tools, Redmond WA 98052
+// Copyright (c) 1998-2021
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+// https://www.geometrictools.com/License/Boost/LICENSE_1_0.txt
+// Version: 5.15.2021.11.28
+
+#pragma once
+
+#include <Mathematics/Hypersphere.h>
+#include <Mathematics/Vector2.h>
+
+// This file provides an implementation of the algorithm in Section 8.7 of the
+// book
+//    Geometric Tools for Computer Graphics,
+//    Philip J. Schneider and David H. Eberly,
+//    Morgan Kaufmnn, San Francisco CA, 2002
+//
+// Given a point P, a radius r and a line Dot(N,X-A) = 0, where A is a point
+// on the line and N is a unit-length normal to the line. Compute the centers
+// of circles, each containing the point, having the specified radius and have
+// the line as a tangent line. The book describes one algebraic approach to
+// solving the problem. The implementation here is another approach, a portion
+// using the algorithm of Section 8.6.
+// 
+// Let N = (n0,n1) and define the unit-length perpendicular D = Perp(N) =
+// (-n1,n0). Represent P = A+u*D+s*N with parameters u = Dot(D,P-A) and
+// s = Dot(N,P-A). The parameter s is the signed distance from P to the line.
+// To simplify the logic of the implementation, if s < 0, the values of s, N
+// and D are negated. The discussion below assumes s >= 0.
+// 
+// The cases are
+//
+//   (1) s = 0: P is on the line. There are two circles containing P and
+//       tangent to the line at P. This is shown in the left image of Figure
+//       8.13. The circle centers are C0 = P-r*N and C1 = P+r*N.
+//
+//   (2) s = r: The book does not have a figure to illustrate this case. The
+//       two circles have a single point of intersection, which is P. The
+//       circle centers are C0 = P-r*D and C1 = P+r*D.
+//
+//   (3) s = 2*r: P is the farthest point on a circle of radius r which has
+//       the line as the tangent line. The circle center is C0 = P-r*N.
+//
+//   (4) s > 2*r: The distance from P to the tangent line is larger than the
+//       desired circle diameter, so there is no circle that satisfies the
+//       constraints.
+//
+//   (5a) 0 < s < r: This is shown in Figure 8.12. Observe that the two
+//        circles intersect in P. There is another point of intersection Q
+//        that is not labeled in the figure. We can represent
+//        Q = P+u*D+(2*r-s)*N. The bisector of segment <P,Q> has origin
+//        (P+Q)/2 = P+u*D+r*N. The bisector direction is D. If a circle center
+//        is C, the triangle <P,B,C> is a right triangle at B. Using the
+//        Pythagorean theorem, the length of segment <B,C> is h = |B-C| =
+//        sqrt(r^2 - (r-s)^2). The circle centers are C0 = B-h*D and
+//        C1 = B+h*D.
+//
+//   (5b) r < s < 2*r: This is analogous to (5a). Figure 8.12 still applies
+//        except that Q must be the label on the intersection point closest to
+//        the tangent line and P must be the label on the intersection point
+//        farthest from the tangent line. The construction of the centers is
+//        the same as that (5a).
+
+namespace gte
+{
+    // The function returns the number of circles satisfying the constraints.
+    // Any circle[i] with i >= numIntersections has members set to zero.
+    template <typename T>
+    size_t CircleThroughPointSpecifiedTangentAndRadius(Vector2<T> const& P,
+        Vector2<T> const& A, Vector2<T> N, T const& r, std::array<Circle2<T>, 2>& circle)
+    {
+        T const zero = static_cast<T>(0);
+
+        Vector2<T> PmA = P - A;
+        T s = Dot(N, PmA);
+        if (s == zero)
+        {
+            // Case (1).
+            circle[0].center = P - r * N;
+            circle[0].radius = r;
+            circle[1].center = P + r * N;
+            circle[1].radius = r;
+            return 2;
+        }
+
+        if (s < zero)
+        {
+            N = -N;
+            s = -s;
+        }
+
+        if (s == r)
+        {
+            // Case (2).
+            Vector2<T> D = Perp(N);
+            circle[0].center = P - r * D;
+            circle[0].radius = r;
+            circle[1].center = P + r * D;
+            circle[1].radius = r;
+            return 2;
+        }
+
+        T const twoR = static_cast<T>(2) * r;
+        if (s == twoR)
+        {
+            // Case (3).
+            circle[0].center = P - r * N;
+            circle[0].radius = r;
+            circle[1].center = { zero, zero };
+            circle[1].radius = zero;
+            return 1;
+        }
+
+        if (s > twoR)
+        {
+            // Case (4).
+            circle[0].center = { zero, zero };
+            circle[0].radius = zero;
+            circle[1].center = { zero, zero };
+            circle[1].radius = zero;
+            return 0;
+        }
+
+        // The bisector origin is D = Perp(N) and The bisector origin is
+        // B = (P + Q) / 2 = A + t * D + r * N with t = Dot(D, P - A).
+        Vector2<T> bisectorDirection = Perp(N);
+        T t = Dot(bisectorDirection, PmA);
+        Vector2<T> bisectorOrigin = A + t * bisectorDirection + r * N;
+
+        T diffRS = r - s;
+        T argument = r * r - diffRS * diffRS;
+        if (argument > zero)
+        {
+            T h = std::sqrt(argument);
+            circle[0].center = bisectorOrigin - h * bisectorDirection;
+            circle[0].radius = r;
+            circle[1].center = bisectorOrigin + h * bisectorDirection;
+            circle[1].radius = r;
+            return 2;
+        }
+        else
+        {
+            // Theoretically this code cannot be reached, but floating-point
+            // rounding errors might trigger it. This corresponds to Case (3)
+            // where r = s.
+            circle[0].center = bisectorOrigin;
+            circle[0].radius = r;
+            circle[1].center = { zero, zero };
+            circle[1].radius = zero;
+            return 1;
+        }
+    }
+}
---
+++ GTE/Mathematics/CircleThroughTwoPointsSpecifiedRadius.h
@@ -0,0 +1,90 @@
+// David Eberly, Geometric Tools, Redmond WA 98052
+// Copyright (c) 1998-2021
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+// https://www.geometrictools.com/License/Boost/LICENSE_1_0.txt
+// Version: 5.15.2021.11.28
+
+#pragma once
+
+#include <Mathematics/Hypersphere.h>
+#include <Mathematics/Vector2.h>
+
+// This file provides an implementation of the algorithm in Section 8.6 of the
+// book
+//    Geometric Tools for Computer Graphics,
+//    Philip J. Schneider and David H. Eberly,
+//    Morgan Kaufmnn, San Francisco CA, 2002
+//
+// Given two distinct points P and Q and given a radius r, compute the centers
+// of circles, each containing the points and having the specified radius.
+//
+// The book states that the circle centers are the points of intersection of
+// circles |X-P|^2 = r^2 and |X-Q|^2 = r^2. The pseudocode simply calls a
+// function to compute these intersections.
+//
+// In my opinion, a simpler approach uses the fact that the bisector of the
+// line segment with endpoints P and Q is a line that contains the centers.
+// The bisector is parameterized by X(t) = t*Perp(P-Q)+(P+Q)/2, where
+// Perp(P-Q) is perpendicular to P-Q and has the same length as that of P-Q.
+// We need values of t for which X(t)-P has length r,
+//   X(t)-P = t*Perp(P - Q)-(P-Q)/2
+//   r^2 = |X(t)-P|^2
+//       = |t*Perp(P-Q)-(P-Q)/2|^2
+//       = |Perp(P-Q)|^2 * t^2 - 2*t*Dot(Perp(P-Q),P-Q) + |P-Q|^2/4
+//       = |P-Q|^2 * t^2 + |P-Q|^2/4
+//       = |P-Q|^2 * (t^2 + 1/4)
+// Observe that t^2+1/4 >= 1/4, which implies that r >= |P-Q|/2. This
+// condition is clear geometrically. The radius must be at least half the
+// length of the segment connecting P and Q.
+//
+// If r = |P-Q|/2, there is a single circle with center (P+Q)/2. If
+// r > |P-Q|/2, there are two circles whose centers occur when
+// t^2 = r^2/|P-Q|^2 - 1/4, which implies t = +/- sqrt(r^2/|P-Q|^2-1/4).
+
+namespace gte
+{
+    // The function returns the number of circles satisfying the constraints.
+    // The number is 2 when r > |P-Q|/2, 1 when r = |P-Q|/2 or 0 when P = Q
+    // or r < |P-Q|/2. Any circle[i] with i >= numIntersections has members
+    // set to zero.
+    template <typename T>
+    size_t CircleThroughTwoPointsSpecifiedRadius(Vector2<T> const& P,
+        Vector2<T> const& Q, T const& r, std::array<Circle2<T>, 2>& circle)
+    {
+        T const zero = static_cast<T>(0);
+
+        Vector2<T> PmQ = P - Q;
+        T sqrLengthPmQ = Dot(PmQ, PmQ);
+        if (sqrLengthPmQ != zero)
+        {
+            T argument = r * r / sqrLengthPmQ - static_cast<T>(0.25);
+            if (argument > zero)
+            {
+                T root = std::sqrt(argument);
+                Vector2<T> bisectorOrigin = static_cast<T>(0.5) * (P + Q);
+                Vector2<T> bisectorDirection = Perp(PmQ);
+                circle[0].center = bisectorOrigin - root * bisectorDirection;
+                circle[0].radius = r;
+                circle[1].center = bisectorOrigin + root * bisectorDirection;
+                circle[1].radius = r;
+                return 2;
+            }
+
+            if (argument == zero)
+            {
+                circle[0].center = static_cast<T>(0.5) * (P + Q);
+                circle[0].radius = r;
+                circle[1].center = { zero, zero };
+                circle[1].radius = zero;
+                return 1;
+            }
+        }
+
+        circle[0].center = { zero, zero };
+        circle[0].radius = zero;
+        circle[1].center = { zero, zero };
+        circle[1].radius = zero;
+        return 0;
+    }
+}
