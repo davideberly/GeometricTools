b8740090ce4c5907cca4df13c2d0931db331cb04
David Eberly <deberly@geometrictools.com>
2022-02-01 10:45:25 -0800

    Update DistTriangle3Triangle3.h
    Updated a comment to be consistent with other distance-query comments.

--- GTE/Mathematics/DistTriangle3Triangle3.h
+++ GTE/Mathematics/DistTriangle3Triangle3.h
@@ -3,7 +3,7 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 // https://www.geometrictools.com/License/Boost/LICENSE_1_0.txt
-// Version: 6.0.2022.01.06
+// Version: 6.0.2022.02.01
 
 #pragma once
 
@@ -16,10 +16,10 @@
 // sum_{i=0}^2 b[i] = 1.
 // 
 // The closest point on triangle0 is stored in closest[0] with barycentric
-// coordinates (b[0],b[1],b[2]) relative to its vertices. The closest point on
-// triangle1 is stored in closest[1] with barycentric coordinates relative to
-// its vertices. When there are infinitely many choices for the pair of closest
-// points, only one of them is returned.
+// coordinates relative to its vertices. The closest point on triangle1 is
+// stored in closest[1] with barycentric coordinates relative to its vertices.
+// When there are infinitely many choices for the pair of closest points, only
+// one pair is returned.
 
 namespace gte
 {
