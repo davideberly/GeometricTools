394e2af77f217adfeed3d2ec331879067e5f9ae9
David Eberly <deberly@geometrictools.com>
2022-03-22 11:09:30 -0700

    Fixed minor version in file versions.
    Fixed the minor number in file versions. Looks like versioning is the bane of the month.

--- GTE/Mathematics/ApprCone3EllipseAndPoints.h
+++ GTE/Mathematics/ApprCone3EllipseAndPoints.h
@@ -3,7 +3,7 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 // https://www.geometrictools.com/License/Boost/LICENSE_1_0.txt
-// Version: 6.0.2022.02.25
+// Version: 6.2.2022.02.25
 
 #pragma once
 
