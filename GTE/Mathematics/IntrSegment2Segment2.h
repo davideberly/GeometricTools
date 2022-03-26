dc6d3a6957f025e40002ea59b97cacc6eed9267c
David Eberly <deberly@geometrictools.com>
2022-03-25 20:58:43 -0700

    Update IntrSegment2Segment2.h
    The current 2D segment-segment queries use the find-intersection query for two lines, but the segments are converted to center-direction-extent form. The two-point representation has endpoints p0 and p1 and the parameterization is p0 + t  (p1 - p0) for t in [0,1]. The center-direction-extent form is c + s d for |s| <= e. The center is c = (p0+p1)/2, the direction is d = (p1-p0)/|p1-p0| and the extent is e = |p1-p0|/2. The computation of d involves a normalization which generally cannot be computed exactly with rational arithmetic. The computation of e is also not exact. The segment parameters returned from the queries are relative to s, not to t. The old queries are operator() functions and still exist. To allow for exact rational computation, I added new queries named Exact which use the two-point form for segments. [In GTL development, I have been eliminating the use of the segment center-direction-extent form.]

--- GTE/Mathematics/IntrSegment2Segment2.h
+++ GTE/Mathematics/IntrSegment2Segment2.h
@@ -33,6 +33,9 @@ namespace gte
             int32_t numIntersections;
         };
 
+        // This version of the query uses Segment3<T>::GetCenteredForm, which
+        // has a Normalize call. This generates rounding errors, so the query
+        // should be used only with float or double.
         Result operator()(Segment2<T> const& segment0, Segment2<T> const& segment1)
         {
             Result result{};
@@ -86,6 +89,79 @@ namespace gte
 
             return result;
         }
+
+        // This version of the query supports rational arithmetic.
+        Result Exact(Segment2<T> const& segment0, Segment2<T> const& segment1)
+        {
+            Result result{};
+
+            T const zero = static_cast<T>(0);
+            T const one = static_cast<T>(1);
+
+            FIQuery<T, Line2<T>, Line2<T>> llQuery{};
+            Vector2<T> seg0Direction = segment0.p[1] - segment0.p[0];
+            Vector2<T> seg1Direction = segment1.p[1] - segment1.p[0];
+            Line2<T> line0(segment0.p[0], seg0Direction);
+            Line2<T> line1(segment1.p[0], seg1Direction);
+            auto llResult = llQuery(line0, line1);
+            if (llResult.numIntersections == 1)
+            {
+                // The lines are not parallel, so they intersect in a single
+                // point. Test whether the line-line intersection is on the
+                // segments.
+                if (zero <= llResult.line0Parameter[0] && llResult.line0Parameter[1] <= one &&
+                    zero <= llResult.line1Parameter[0] && llResult.line1Parameter[1] <= one)
+                {
+                    result.intersect = true;
+                    result.numIntersections = 1;
+                }
+                else
+                {
+                    result.intersect = false;
+                    result.numIntersections = 0;
+                }
+            }
+            else if (llResult.numIntersections == std::numeric_limits<int32_t>::max())
+            {
+                // The lines are the same. Compute the location of segment1
+                // endpoints relative to segment0.
+                T dotD0D0 = Dot(seg0Direction, seg0Direction);
+                Vector2<T> diff = segment1.p[0] - segment0.p[0];
+                T t0 = Dot(seg0Direction, diff) / dotD0D0;
+                diff = segment1.p[1] - segment0.p[0];
+                T t1 = Dot(seg0Direction, diff) / dotD0D0;
+
+                // Get the parameter intervals of the segments relative to
+                // segment0.
+                std::array<T, 2> interval0 = { zero, one };
+                std::array<T, 2> interval1{};
+                if (t1 >= t0)
+                {
+                    interval1[0] = t0;
+                    interval1[1] = t1;
+                }
+                else
+                {
+                    interval1[0] = t1;
+                    interval1[1] = t0;
+                }
+
+                // Compute the intersection of the intervals.
+                FIQuery<T, std::array<T, 2>, std::array<T, 2>> iiQuery{};
+                auto iiResult = iiQuery(interval0, interval1);
+                result.intersect = iiResult.intersect;
+                result.numIntersections = iiResult.numIntersections;
+            }
+            else
+            {
+                // The lines are parallel but not the same, so the segments
+                // cannot intersect.
+                result.intersect = false;
+                result.numIntersections = 0;
+            }
+
+            return result;
+        }
     };
 
     template <typename T>
@@ -125,6 +201,12 @@ namespace gte
             std::array<Vector2<T>, 2> point;
         };
 
+        // This version of the query uses Segment3<T>::GetCenteredForm, which
+        // has a Normalize call. This generates rounding errors, so the query
+        // should be used only with float or double. NOTE: The parameters are
+        // are relative to the centered form of the segment. Each segment has
+        // a center C, a unit-length direction D and an extent e > 0. A
+        // segment point is C+t*D where |t| <= e.
         Result operator()(Segment2<T> const& segment0, Segment2<T> const& segment1)
         {
             Result result{};
@@ -146,8 +228,11 @@ namespace gte
                     result.intersect = true;
                     result.numIntersections = 1;
                     result.segment0Parameter[0] = llResult.line0Parameter[0];
+                    result.segment0Parameter[1] = result.segment0Parameter[0];
                     result.segment1Parameter[0] = llResult.line1Parameter[0];
+                    result.segment1Parameter[1] = result.segment1Parameter[0];
                     result.point[0] = llResult.point;
+                    result.point[1] = result.point[0];
                 }
                 else
                 {
@@ -179,6 +264,133 @@ namespace gte
                         result.segment0Parameter[i] = iiResult.overlap[i];
                         result.segment1Parameter[i] = iiResult.overlap[i] - t;
                         result.point[i] = seg0Origin + result.segment0Parameter[i] * seg0Direction;
+                    }
+                    if (iiResult.numIntersections == 1)
+                    {
+                        result.segment0Parameter[1] = result.segment0Parameter[0];
+                        result.segment1Parameter[1] = result.segment1Parameter[0];
+                        result.point[1] = result.point[0];
+                    }
+                }
+                else
+                {
+                    result.intersect = false;
+                    result.numIntersections = 0;
+                }
+            }
+            else
+            {
+                result.intersect = false;
+                result.numIntersections = 0;
+            }
+
+            return result;
+        }
+
+        // This version of the query supports rational arithmetic. NOTE: The
+        // parameters are relative to the endpoint form of the segment. Each
+        // segment has endpoints P0 and P1. A segment point is P0+t*(P1-P0)
+        // where 0 <= t <= 1.
+        Result Exact(Segment2<T> const& segment0, Segment2<T> const& segment1)
+        {
+            Result result{};
+
+            T const zero = static_cast<T>(0);
+            T const one = static_cast<T>(1);
+
+            FIQuery<T, Line2<T>, Line2<T>> llQuery{};
+            Vector2<T> seg0Direction = segment0.p[1] - segment0.p[0];
+            Vector2<T> seg1Direction = segment1.p[1] - segment1.p[0];
+            Line2<T> line0(segment0.p[0], seg0Direction);
+            Line2<T> line1(segment1.p[0], seg1Direction);
+            auto llResult = llQuery(line0, line1);
+            if (llResult.numIntersections == 1)
+            {
+                // The lines are not parallel, so they intersect in a single
+                // point. Test whether the line-line intersection is on the
+                // segments.
+                if (zero <= llResult.line0Parameter[0] && llResult.line0Parameter[1] <= one &&
+                    zero <= llResult.line1Parameter[0] && llResult.line1Parameter[1] <= one)
+                {
+                    result.intersect = true;
+                    result.numIntersections = 1;
+                    result.segment0Parameter[0] = llResult.line0Parameter[0];
+                    result.segment0Parameter[1] = result.segment0Parameter[0];
+                    result.segment1Parameter[0] = llResult.line1Parameter[0];
+                    result.segment1Parameter[1] = result.segment1Parameter[0];
+                    result.point[0] = llResult.point;
+                    result.point[1] = result.point[0];
+                }
+                else
+                {
+                    result.intersect = false;
+                    result.numIntersections = 0;
+                }
+            }
+            else if (llResult.numIntersections == std::numeric_limits<int32_t>::max())
+            {
+                // The lines are the same. Compute the location of segment1
+                // endpoints relative to segment0.
+                T dotD0D0 = Dot(seg0Direction, seg0Direction);
+                Vector2<T> diff = segment1.p[0] - segment0.p[0];
+                T t0 = Dot(seg0Direction, diff) / dotD0D0;
+                diff = segment1.p[1] - segment0.p[0];
+                T t1 = Dot(seg0Direction, diff) / dotD0D0;
+
+                // Get the parameter intervals of the segments relative to
+                // segment0.
+                std::array<T, 2> interval0 = { zero, one };
+                std::array<T, 2> interval1{};
+                if (t1 >= t0)
+                {
+                    interval1[0] = t0;
+                    interval1[1] = t1;
+                }
+                else
+                {
+                    interval1[0] = t1;
+                    interval1[1] = t0;
+                }
+
+                // Compute the intersection of the intervals.
+                FIQuery<T, std::array<T, 2>, std::array<T, 2>> iiQuery{};
+                auto iiResult = iiQuery(interval0, interval1);
+                if (iiResult.intersect)
+                {
+                    result.intersect = true;
+                    result.numIntersections = iiResult.numIntersections;
+
+                    // Compute the results for segment0.
+                    for (int32_t i = 0; i < iiResult.numIntersections; ++i)
+                    {
+                        result.segment0Parameter[i] = iiResult.overlap[i];
+                        result.point[i] = segment0.p[0] + result.segment0Parameter[i] * seg0Direction;
+                    }
+
+                    // Compute the results for segment1. The interval1 was
+                    // computed relative to segment0, so we have to reverse
+                    // the process to obtain the parameters.
+                    T dotD1D1 = Dot(seg1Direction, seg1Direction);
+                    for (int32_t i = 0; i < iiResult.numIntersections; ++i)
+                    {
+                        diff = result.point[i] - segment1.p[0];
+                        result.segment1Parameter[i] = Dot(seg1Direction, diff) / dotD1D1;
+                    }
+
+                    if (iiResult.numIntersections == 1)
+                    {
+                        result.segment0Parameter[1] = result.segment0Parameter[0];
+                        result.segment1Parameter[1] = result.segment1Parameter[0];
+                        result.point[1] = result.point[0];
+                    }
+                    else
+                    {
+                        if (t1 < t0)
+                        {
+                            std::swap(
+                                result.segment1Parameter[0],
+                                result.segment1Parameter[1]);
+                        }
                     }
                 }
                 else
