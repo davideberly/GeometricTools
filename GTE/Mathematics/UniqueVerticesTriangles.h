29b08a5e3a3990fb4c3aac78b8743c55f860d597
David Eberly <deberly@geometrictools.com>
2022-06-08 17:54:51 -0700

    Update UniqueVerticesTriangles.h
    Fixed an error in the deprecated file for UniqueVerticesTriangles; the <array> header is not explicitly included. This occurred with gcc 12.1.1 on Fedora 36 even though my instantiation tool did not include explicit instantiation for that class. The error did not occur with gcc 9.3.0 on Ubuntu 20.04.1 LTS or Microsoft compilers when running the tool.

--- GTE/Mathematics/UniqueVerticesTriangles.h
+++ GTE/Mathematics/UniqueVerticesTriangles.h
@@ -3,12 +3,13 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 // https://www.geometrictools.com/License/Boost/LICENSE_1_0.txt
-// Version: 6.0.2022.03.13
+// Version: 6.0.2022.06.08
 
 #pragma once
 
 #include <Mathematics/Logger.h>
 #include <algorithm>
+#include <array>
 #include <map>
 #include <set>
 #include <vector>
