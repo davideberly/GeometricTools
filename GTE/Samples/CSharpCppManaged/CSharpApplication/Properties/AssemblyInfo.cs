1c35f3a5f7f38c75191f03216414a77294f947dd
David Eberly <deberly@geometrictools.com>
2022-07-12 17:59:58 -0700

    Minor updates to the C#/CLI code.
    Updated the copyright date in the assembly file. Modified the comments in the program file.

--- GTE/Samples/CSharpCppManaged/CSharpApplication/Properties/AssemblyInfo.cs
+++ GTE/Samples/CSharpCppManaged/CSharpApplication/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CSharpApplication")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
