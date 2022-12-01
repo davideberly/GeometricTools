d4dbd408646936712e1c4142235212d977ef9391
David Eberly <deberly@geometrictools.com>
2022-12-01 09:53:51 -0800

    New 2D distance queries (point-circle, point-arc)
    Added new 2D distance queries for point-circle and point-arc. Also fixed some
    missing header file references in the GTMathematics.v16 project.

---
+++ GTE/Mathematics/DistPoint2Arc2.h
@@ -0,0 +1,100 @@
+// David Eberly, Geometric Tools, Redmond WA 98052
+// Copyright (c) 1998-2022
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+// https://www.geometrictools.com/License/Boost/LICENSE_1_0.txt
+// Version: 6.5.2022.12.01
+
+#pragma once
+
+#include "DistPoint2Circle2.h"
+#include <Mathematics/Arc2.h>
+
+namespace gte
+{
+    template <typename T>
+    class DCPQuery<T, Vector2<T>, Arc2<T>>
+    {
+    public:
+        // The input point is stored in the member closest[0]. If a single
+        // point on the arc is closest to the input point, the member
+        // closest[1] is set to that point and the equidistant member is set
+        // to false. If the entire arc is equidistant to the point, the member
+        // closest[1] is set to the endpoint E0 of the arc and the equidistant
+        // member is set to true.
+        struct Result
+        {
+            Result()
+                :
+                distance(static_cast<T>(0)),
+                sqrDistance(static_cast<T>(0)),
+                closest{ Vector2<T>::Zero(), Vector2<T>::Zero() },
+                equidistant(false)
+            {
+            }
+
+            T distance, sqrDistance;
+            std::array<Vector2<T>, 2> closest;
+            bool equidistant;
+        };
+
+        Result operator()(Vector2<T> const& point, Arc2<T> const& arc)
+        {
+            Result result{};
+
+            Circle2<T> circle(arc.center, arc.radius);
+            DCPQuery<T, Vector2<T>, Circle2<T>> pcQuery{};
+            auto pcResult = pcQuery(point, circle);
+            if (!pcResult.equidistant)
+            {
+                // Test whether the closest circle point is on the arc. If it
+                // is, that point is the closest arc point. If it is not, the
+                // closest arc point is an arc endpoint. Determine which
+                // endpoint that is.
+                if (arc.Contains(pcResult.closest[1]))
+                {
+                    result.distance = pcResult.distance;
+                    result.sqrDistance = pcResult.sqrDistance;
+                    result.closest = pcResult.closest;
+                    result.equidistant = pcResult.equidistant;
+                }
+                else
+                {
+                    Vector2<T> diff0 = arc.end[0] - point;
+                    Vector2<T> diff1 = arc.end[1] - point;
+                    T sqrLength0 = Dot(diff0, diff0);
+                    T sqrLength1 = Dot(diff1, diff1);
+                    T length0 = std::sqrt(sqrLength0);
+                    T length1 = std::sqrt(sqrLength1);
+                    if (length0 <= length1)
+                    {
+                        result.distance = length0;
+                        result.sqrDistance = sqrLength0;
+                        result.closest[0] = point;
+                        result.closest[1] = arc.end[0];
+                        result.equidistant = false;
+                    }
+                    else
+                    {
+                        result.distance = length1;
+                        result.sqrDistance = sqrLength1;
+                        result.closest[0] = point;
+                        result.closest[1] = arc.end[1];
+                        result.equidistant = false;
+                    }
+                }
+            }
+            else
+            {
+                // The point is the center of the circle containing the arc.
+                result.distance = arc.radius;
+                result.sqrDistance = arc.radius * arc.radius;
+                result.closest[0] = point;
+                result.closest[1] = arc.end[0];
+                result.equidistant = true;
+            }
+
+            return result;
+        }
+    };
+}
---
+++ GTE/Mathematics/DistPoint2Circle2.h
@@ -0,0 +1,71 @@
+// David Eberly, Geometric Tools, Redmond WA 98052
+// Copyright (c) 1998-2022
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+// https://www.geometrictools.com/License/Boost/LICENSE_1_0.txt
+// Version: 6.5.2022.12.01
+
+#pragma once
+
+#include <Mathematics/DCPQuery.h>
+#include <Mathematics/Hypersphere.h>
+#include <Mathematics/Vector2.h>
+
+namespace gte
+{
+    template <typename T>
+    class DCPQuery<T, Vector2<T>, Circle2<T>>
+    {
+    public:
+        // The input point is stored in the member closest[0]. If a single
+        // point on the circle is closest to the input point, the member
+        // closest[1] is set to that point and the equidistant member is set
+        // to false. If the entire circle is equidistant to the point, the
+        // member closest[1] is set to C+r*(1,0), where C is the circle
+        // center and r is the circle radius. Moreover, the equidistant
+        // member is set to true.
+        struct Result
+        {
+            Result()
+                :
+                distance(static_cast<T>(0)),
+                sqrDistance(static_cast<T>(0)),
+                closest{ Vector2<T>::Zero(), Vector2<T>::Zero() },
+                equidistant(false)
+            {
+            }
+
+            T distance, sqrDistance;
+            std::array<Vector2<T>, 2> closest;
+            bool equidistant;
+        };
+
+        Result operator()(Vector2<T> const& point, Circle2<T> const& circle)
+        {
+            Result result{};
+
+            Vector2<T> diff = point - circle.center;
+            T sqrLength = Dot(diff, diff);
+            T length = std::sqrt(sqrLength);
+            if (length > static_cast<T>(0))
+            {
+                diff /= length;
+                result.distance = std::fabs(length - circle.radius);
+                result.sqrDistance = result.distance * result.distance;
+                result.closest[0] = point;
+                result.closest[1] = circle.center + circle.radius * diff;
+                result.equidistant = false;
+            }
+            else
+            {
+                result.distance = circle.radius;
+                result.sqrDistance = circle.radius * circle.radius;
+                result.closest[0] = point;
+                result.closest[1] = circle.center + circle.radius * Vector2<T>::Unit(0);
+                result.equidistant = true;
+            }
+
+            return result;
+        }
+    };
+}
