ada0e20fd33ecd9cf8a20b766cac214adc5514ef
David Eberly <deberly@geometrictools.com>
2023-07-23 13:12:38 -0700

    Fixed bug in DistLine3Circle3.
    The code ported to GTL had a unit-test failure for the Robust() function in the case that the circle center was not the origin. The GTE unit tests did not contain such a case. One block of Robust() was missing the translation of the closest line point to the original coordinate system.

--- GTE/Mathematics/DistLine3Circle3.h
+++ GTE/Mathematics/DistLine3Circle3.h
@@ -3,7 +3,7 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 // https://www.geometrictools.com/License/Boost/LICENSE_1_0.txt
-// Version: 6.0.2023.05.22
+// Version: 6.0.2023.07.23
 
 #pragma once
 
@@ -297,6 +297,7 @@ namespace gte
                     {
                         GetClosestPair(line.direction, oldD, circle.normal, circle.radius, t,
                             info.lineClosest, info.circleClosest);
+                        info.lineClosest += circle.center;
                         info.circleClosest += circle.center;
                         info.equidistant = false;
                     }
