8c6e1890d881600134ce87ea37f623c199c703f1
deber <deberly@geometrictools.com>
2023-10-21 17:05:52 -0700

    Modified STLBinaryFile.h.
    Added code to save a binary STL file.

--- GTE/Mathematics/STLBinaryFile.h
+++ GTE/Mathematics/STLBinaryFile.h
@@ -3,11 +3,11 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 // https://www.geometrictools.com/License/Boost/LICENSE_1_0.txt
-// Version: 6.9.2023.10.20
+// Version: 6.9.2023.10.21
 
 #pragma once
 
-// A loader for binary STL files. The file format is described at
+// A reader/writer for binary STL files. The file format is described at
 // https://en.wikipedia.org/wiki/STL_(file_format).
 //
 // The class Tuple3 must represent 3 contiguous float-valued numbers where
@@ -88,6 +88,49 @@ namespace gte
             }
 
             input.close();
+            return true;
+        }
+
+        // The caller is responsible for populating the 'header' and
+        // 'triangles' members of the STLBinaryFile object.
+        bool Save(std::string const& filename)
+        {
+            std::ofstream output(filename, std::ios::binary);
+            if (!output)
+            {
+                return false;
+            }
+
+            if (output.write((char const*)header.data(), header.size()).fail())
+            {
+                return false;
+            }
+
+            uint32_t numTriangles = static_cast<uint32_t>(triangles.size());
+            if (output.write((char const*)&numTriangles, sizeof(numTriangles)).fail())
+            {
+                return false;
+            }
+
+            for (auto const& triangle : triangles)
+            {
+                if (output.write((char const*)&triangle.normal, sizeof(triangle.normal)).fail())
+                {
+                    return false;
+                }
+
+                if (output.write((char const*)triangle.vertex.data(), triangle.vertex.size() * sizeof(triangle.vertex[0])).fail())
+                {
+                    return false;
+                }
+
+                if (output.write((char const*)&triangle.attributeByteCount, sizeof(triangle.attributeByteCount)).fail())
+                {
+                    return false;
+                }
+            }
+
+            output.close();
             return true;
         }
 
