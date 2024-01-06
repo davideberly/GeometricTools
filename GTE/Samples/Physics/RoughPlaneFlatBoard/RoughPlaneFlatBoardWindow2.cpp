4e22a7578435cd15c5674ea8d4a5368daf6c03dd
deberly <deberly@geometrictools.com>
2024-01-06 09:55:18 -0800

    Minor fix, added preprocessor symbol.
    The preprocessor symbol declaration SINGLE_STEP was missing from the files. By enabling this, you can single step through the simulation by pressing the 'g' key.

--- GTE/Samples/Physics/RoughPlaneFlatBoard/RoughPlaneFlatBoardWindow2.cpp
+++ GTE/Samples/Physics/RoughPlaneFlatBoard/RoughPlaneFlatBoardWindow2.cpp
@@ -3,9 +3,11 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 // https://www.geometrictools.com/License/Boost/LICENSE_1_0.txt
-// Version: 6.1.2022.01.10
+// Version: 6.1.2024.01.06
 
 #include "RoughPlaneFlatBoardWindow2.h"
+
+//#define SINGLE_STEP
 
 RoughPlaneFlatBoardWindow2::RoughPlaneFlatBoardWindow2(Parameters& parameters)
     :
