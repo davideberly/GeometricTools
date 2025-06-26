242fa55bd0df0c51765da35799ffda0a19be5ba0
CodeReclaimers <CodeReclaimers@users.noreply.github.com>
2025-06-26 12:01:14 -0400

    Update version

--- GTE/Mathematics/DistAlignedBoxAlignedBox.h
+++ GTE/Mathematics/DistAlignedBoxAlignedBox.h
@@ -3,7 +3,7 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 // https://www.geometrictools.com/License/Boost/LICENSE_1_0.txt
-// File Version: 8.0.2025.05.10
+// File Version: 8.0.2025.06.26
 
 #pragma once
 
