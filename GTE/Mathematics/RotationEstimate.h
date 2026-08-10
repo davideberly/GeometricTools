db8560d62cb332b19037e0aa552f907acd2740cd
David Eberly <deberly@geometrictools.com>
2026-08-10 16:20:43 -0700

    Update RotationEstimate.h
    Defect found by Claude Sonnet5/Fable5. The degree-16 constants in
    C_ROTC4_EST_COEFF} have an entry +2.505210838484547384e-08 which is missing a terminating constant. The compiler adds the next constant to it, in which case the last constant of the array is implicitly zero. I added the comma.

--- GTE/Mathematics/RotationEstimate.h
+++ GTE/Mathematics/RotationEstimate.h
@@ -3,7 +3,7 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 // https://www.geometrictools.com/License/Boost/LICENSE_1_0.txt
-// File Version: 8.0.2025.05.10
+// File Version: 8.0.2026.08.10
 
 #pragma once
 
@@ -360,7 +360,7 @@ namespace gte
             -8.333333333333333331e-03,
             +1.984126984126983769e-04,
             -2.755731922398382640e-06,
-            +2.505210838484547384e-08
+            +2.505210838484547384e-08,
             -1.605904373998917420e-10,
             +7.647154485773692262e-13,
             -2.810942149025141692e-15,
